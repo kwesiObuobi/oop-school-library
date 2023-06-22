2947a699a8a3cc58536a91616e078d8e173ac95b
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-06-22 12:35:55 +0000

    Update attribute accessors for files

--- person.rb
+++ person.rb
@@ -1,8 +1,8 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id, :rentals
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -22,7 +22,7 @@ class Person < Nameable
   end
 
   def add_rental(book, date)
-    Rental.new(date, book, self)
+    Rental.new(date, self, book)
   end
 
   private
