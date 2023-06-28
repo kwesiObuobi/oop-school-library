9d98440c0dff5447ab3ae135498547165f5a2aa7
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-06-28 10:57:44 +0000

    Add method to load 'people' data from people.json into @people instance variable

--- app.rb
+++ app.rb
@@ -7,23 +7,41 @@ require 'json'
 class App
   def initialize
     @books = load_books
-    @people = []
+    @people = load_people
     @rentals = []
   end
 
   def load_file_content(file)
     filename = "data/#{file}.json"
     return [] unless File.exist?(filename)
+
     content = File.read(filename)
     JSON.parse(content)
   end
 
   def load_books
     books = load_file_content('books')
-    return [] unless books.length > 0
+    return [] unless books.length.positive?
+
     retrieved_books = []
     books.each { |book| retrieved_books << Book.new(book['title'], book['author']) }
     retrieved_books
+  end
+
+  def load_people
+    people = load_file_content('people')
+    return [] unless people.length.positive?
+
+    retrieved_people = []
+    people.each do |person|
+      retrieved_people << if person['person'] == 'Teacher'
+                            Teacher.new(person['specialization'], person['age'], person['name'])
+                          else
+                            Student.new(nil, person['age'], person['name'],
+                                        parent_permission: person['parent_permission'])
+                          end
+    end
+    retrieved_people
   end
 
   # 0
